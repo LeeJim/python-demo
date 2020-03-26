6695356c9308eb84f4f067e383cecb5af3e0ee57
leejimqiu <leejimqiu@wesure.cn>
2020-03-26 14:16:19 +0800

    对回答做去重；过滤部分回答的壁纸信息

--- format.py
+++ format.py
@@ -2,10 +2,17 @@ import json
 
 d = open('./imgs.json', encoding='utf-8')
 s = ''
-
+has_watermark = set()
+has_watermark.add('789763565')
 for i in json.load(d):
-    s += json.dumps(i)
-    s += '\n'
+    ratio = float(i['height']) / float(i['width'])
+    i['ratio'] = round(ratio, 1)
+    cond = i['answerId'] not in has_watermark
+    if ratio > 1 and ratio < 3 and cond:
+        s += json.dumps(i)
+        s += '\n'
+    else:
+        print(i['src'])
 
 with open('raw.json', 'w') as f:
     f.write(s)
--- spider.py
+++ spider.py
@@ -3,7 +3,6 @@ from urllib.parse import urljoin
 import re
 import requests
 import json
-
 from bs4 import BeautifulSoup
 
 def main():
@@ -16,6 +15,17 @@ def main():
     def sortByVote(i):
         return i['upvoteNum']
     data.sort(key=sortByVote, reverse=True)
+
+    # 去重
+    def unique(items, key=None):
+        seen = set()
+        for item in items:
+            val = item if key is None else key(item)
+            if val not in seen:
+                yield item
+                seen.add(val)
+    data = list(unique(data, key=lambda d: d['id']))
+
     imgList = []
     print('total: %d' %(len(data)))
     for i in data:
@@ -23,7 +33,7 @@ def main():
         resp = requests.get(seed_url,
                             headers=headers)
         soup = BeautifulSoup(resp.text, 'lxml')
-        for img in soup.find_all('img'):
+        for img in soup.find_all('img', class_="lazy"): # 过滤noscript的部分
             if img.has_attr('data-original'):
                 imgList.append({
                     'answerId': i['id'],
